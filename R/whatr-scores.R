139bcb4666c6898d28f08d1610f9262464e697f7
Kiernan Nicholls <kiernann@protonmail.com>
2020-02-19 10:49:15 -0500

    Update whatr_scores()

--- R/whatr-scores.R
+++ R/whatr-scores.R
@@ -1,84 +1,90 @@
-#' Scrape Jeopardy game score history
+#' What are player scores?
 #'
-#' Return the tidy tibble of contestant scores over the course of the game.
-#' The tibble indicates what questions were answered correctly and the change
-#' in score from each answer.
+#' This data frame has data on the performance of each contestant.
 #'
+#' @param html An HTML document from [read_scores()].
 #' @param game The J-Archive! game ID number.
-#' @param date The original date an episode aired.
-#' @param show The sequential show number.
-#' @return Tidy tibble of an episode score history.
-#' @family J! scrapers
+#' @return Tidy tibble of clue scores.
+#' @format A tibble with 52 rows and 8 variables:
+#' \describe{
+#'   \item{round}{The round a clue is chosen.}
+#'   \item{n}{The order of clue chosen.}
+#'   \item{name}{First name of player responding.}
+#'   \item{score}{Change in score from this clue.}
+#'   \item{double}{Is the clue a daily double.}
+#' }
 #' @examples
 #' whatr_scores(game = 6304)
-#' whatr_scores(date = "2019-06-03")
-#' whatr_scores(show = 8006)
-#' @importFrom dplyr arrange bind_rows group_by left_join mutate row_number select slice starts_with ungroup lag
-#' @importFrom httr GET
+#' read_scores(6304) %>% whatr_scores()
+#' @importFrom dplyr arrange bind_rows group_by mutate select slice ungroup lag
 #' @importFrom rvest html_node html_nodes html_table html_text
-#' @importFrom stringr str_c str_extract str_remove str_remove_all
-#' @importFrom tibble as_tibble enframe
+#' @importFrom stringr str_remove_all
+#' @importFrom tibble as_tibble
 #' @importFrom tidyr drop_na pivot_longer
-#' @importFrom xml2 read_html
 #' @export
-whatr_scores <- function(game = NULL, date = NULL, show = NULL) {
-  game <- whatr_id(game, date, show)
-  showscores <- xml2::read_html(paste0("http://www.j-archive.com/showscores.php?game_id=", game))
+whatr_scores <- function(html = NULL, game = NULL) {
+  # read showscore html
+  if (is.null(html)) {
+    showscores <- read_scores(game)
+  } else {
+    showscores <- html
+  }
 
+  # find round 1 doubles location
   single_doubles <- showscores %>%
     rvest::html_node("#jeopardy_round > table td.ddred") %>%
     rvest::html_text() %>%
     base::as.integer()
 
-  tibble_uniqe <- function(x) {
-    suppressMessages(tibble::as_tibble(x, .name_repair = "unique"))
-  }
-
+  # pivot round 1 scores
   single_score <- showscores %>%
     rvest::html_node("#jeopardy_round > table:nth-child(2)") %>%
     rvest::html_table(fill = TRUE, header = TRUE) %>%
-    tibble_uniqe() %>%
-    dplyr::select(-dplyr::starts_with("...")) %>%
-    dplyr::mutate(n = dplyr::row_number(), round = 1L) %>%
+    magrittr::extract(, 2:4) %>%
+    tibble::as_tibble() %>%
+    dplyr::mutate(n = seq(1L, nrow(.)), round = 1L) %>%
     tidyr::pivot_longer(
       cols = -c(n, round),
       names_to = "name",
       values_to = "score"
     ) %>%
     dplyr::mutate(
-      score = as.integer(stringr::str_remove_all(score, "\\$|\\,")),
+      score = as.integer(stringr::str_remove_all(score, "[^\\d]")),
       double = (n == single_doubles)
     )
 
+  # find round 2 doubles location
   double_doubles <- showscores %>%
     rvest::html_nodes("#double_jeopardy_round > table td.ddred") %>%
     rvest::html_text() %>%
-    base::unique() %>%
-    base::as.integer()
+    base::as.integer() %>%
+    base::unique()
 
+  # pivot round 2 scores
   double_score <- showscores %>%
     rvest::html_node("#double_jeopardy_round > table:nth-child(2)") %>%
-    rvest::html_table(header = TRUE, fill = TRUE) %>%
+    rvest::html_table(fill = TRUE, header = TRUE) %>%
     tidyr::drop_na() %>%
-    tibble_uniqe() %>%
-    dplyr::select(-dplyr::starts_with("...")) %>%
-    dplyr::mutate(n = dplyr::row_number(), round = 2) %>%
+    magrittr::extract(, 2:4) %>%
+    tibble::as_tibble() %>%
+    dplyr::mutate(n = seq(1L, nrow(.)), round = 1L) %>%
     tidyr::pivot_longer(
       cols = -c(n, round),
       names_to = "name",
       values_to = "score"
     ) %>%
     dplyr::mutate(
-      score = as.integer(stringr::str_remove_all(score, "\\$|\\,")),
+      score = as.integer(stringr::str_remove_all(score, "[^\\d]")),
       double = (n %in% double_doubles),
       n = n + max(single_score$n)
     )
 
+  # pivot round 3 scores
   final_scores <- showscores %>%
     rvest::html_node("#final_jeopardy_round > table:nth-child(2)") %>%
     rvest::html_table(header = TRUE, fill = TRUE) %>%
     dplyr::slice(1) %>%
-    tibble_uniqe() %>%
+    tibble::as_tibble() %>%
     dplyr::mutate(
       n = max(double_score$n) + 1L,
       round = 3L
@@ -88,19 +94,21 @@ whatr_scores <- function(game = NULL, date = NULL, show = NULL) {
       names_to = "name",
       values_to = "score"
     ) %>%
-    dplyr::mutate(score = as.integer(stringr::str_remove_all(score, "\\$|\\,")))
+    dplyr::mutate(
+      score = as.integer(stringr::str_remove_all(score, "[^\\d]")),
+      double = FALSE
+    )
 
+  # bind all scores and tidy
   scores <- single_score %>%
     dplyr::bind_rows(double_score) %>%
     dplyr::bind_rows(final_scores) %>%
-    dplyr::arrange(round, n) %>%
-    dplyr::select(round, n, name, score, double) %>%
     dplyr::group_by(name) %>%
-    dplyr::mutate(
-      change = ifelse(n == 1, score, score - dplyr::lag(score)),
-      correct = ifelse(change == 0, NA, change > 0)
-    ) %>%
-    dplyr::ungroup()
+    dplyr::mutate(score = ifelse(n == 1, score, score - dplyr::lag(score))) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(score != 0 | n == 1) %>%
+    dplyr::arrange(round, n) %>%
+    dplyr::select(round, n, name, score, double)
 
   return(scores)
 }
