07a31951acda0056c05eac5c001122b70c5202c3
Kiernan Nicholls <kiernann@protonmail.com>
2020-02-26 12:23:30 -0500

    Use new input for summary

--- R/whatr-summary.R
+++ R/whatr-summary.R
@@ -3,8 +3,7 @@
 #' _These_ must be given by the contestants in the form of a question in
 #' response to the clues asked.
 #'
-#' @param html An HTML document from [read_game()].
-#' @param game The J-Archive! game ID number, possibly from [whatr_id()].
+#' @inheritParams whatr_scores
 #' @return A tidy tibble of clue text.
 #' @format A tibble with (usually) 3 row and 5 variables:
 #' \describe{
@@ -16,15 +15,16 @@
 #' }
 #' @examples
 #' whatr_summary(game = 6304)
+#' whatr_html(6304) %>% whatr_summary()
 #' @export
-whatr_summary <- function(html = NULL, game = NULL) {
-  if (is.null(html)) {
-    showgame <- read_game(game)
-  } else {
-    showgame <- html
+whatr_summary <- function(game) {
+  if (is(game, "xml_document") & grepl("ddred", as.character(game), )) {
+    stop("a 'showgame' HTML input is needed")
+  } else if (!is(game, "xml_document")) {
+    game <- whatr_html(x = game, out = "showgame")
   }
 
-  coryat_final <- showgame %>%
+  coryat_final <- game %>%
     rvest::html_node("#final_jeopardy_round > table:nth-child(8)") %>%
     rvest::html_table(header = TRUE, fill = TRUE) %>%
     tibble::as_tibble() %>%
@@ -36,7 +36,7 @@ whatr_summary <- function(html = NULL, game = NULL) {
         stringr::str_remove(",") %>%
         base::as.integer()
     )
-  final_final <- showgame %>%
+  final_final <- game %>%
     rvest::html_node("#final_jeopardy_round > table:nth-child(4)") %>%
     rvest::html_table(header = TRUE, fill = TRUE) %>%
     tibble::as_tibble() %>%
@@ -48,7 +48,7 @@ whatr_summary <- function(html = NULL, game = NULL) {
         stringr::str_remove(",") %>%
         base::as.integer()
     )
-  right_wrong <- showgame %>%
+  right_wrong <- game %>%
     rvest::html_node("#final_jeopardy_round > table:nth-child(8)") %>%
     rvest::html_table(header = TRUE, fill = TRUE) %>%
     tibble::as_tibble() %>%
