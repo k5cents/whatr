3c1b7c68eb88faad895c3c3a2914bbbc4d37d39e
Kiernan Nicholls <kiernann@protonmail.com>
2020-02-26 12:21:18 -0500

    Use new input for info

--- R/whatr-info.R
+++ R/whatr-info.R
@@ -3,8 +3,7 @@
 #' _These_ must be given by the contestants in the form of a question in
 #' response to the clues asked.
 #'
-#' @param html An HTML document from [read_game()].
-#' @param game The J-Archive! game ID number, possibly from [whatr_id()].
+#' @inheritParams whatr_scores
 #' @return A tidy tibble of clue text.
 #' @format A tibble with 1 row and 3 variables:
 #' \describe{
@@ -14,23 +13,23 @@
 #' }
 #' @examples
 #' whatr_info(game = 6304)
-#' read_game(6304) %>% whatr_info()
+#' whatr_html(6304) %>% whatr_info()
 #' @importFrom rvest html_node html_text
 #' @importFrom stringr str_extract
 #' @importFrom tibble tibble
 #' @export
-whatr_info <- function(html = NULL, game = NULL) {
-  if (is.null(html)) {
-    showgame <- read_game(game)
-  } else {
-    showgame <- html
-    game <- as.character(html) %>%
-      str_extract("(?<=chartgame.php\\?game_id\\=)\\d+")
+whatr_info <- function(game) {
+  if (is(game, "xml_document") & grepl("ddred", as.character(game), )) {
+    stop("a 'showgame' HTML input is needed")
+  } else if (!is(game, "xml_document")) {
+    game <- whatr_html(x = game, out = "showgame")
   }
 
-  title <- rvest::html_text(rvest::html_node(showgame, "title"))
+  c <- as.character(game)
+  id <- stringr::str_extract(c, "(?<=chartgame.php\\?game_id\\=)\\d+")
+  title <- rvest::html_text(rvest::html_node(game, "title"))
   info <- tibble::tibble(
-    game = as.integer(game),
+    game = as.integer(id),
     show = as.integer(stringr::str_extract(title, "(\\d+)")),
     date = as.Date(stringr::str_extract(title, "\\d+-\\d+-\\d+$"))
   )
