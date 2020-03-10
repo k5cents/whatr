c6c6cdc3c538d01a62763bbf4028fb8e1896305a
Kiernan Nicholls <kiernann@protonmail.com>
2020-03-10 15:10:40 -0400

    Remove whatr_id()

--- R/whatr-html.R
+++ R/whatr-html.R
@@ -1,33 +1,3 @@
-#' Return a J! Archive game ID
-#'
-#' Return the game ID for a show air date or number. The game ID is needed to
-#' return the "showgame" or "showscores" page.
-#'
-#' @param game The J! Archive game ID number.
-#' @param date The original date an episode aired.
-#' @param show The sequential show number.
-#' @return The J! Archive game ID number.
-#' @examples
-#' whatr_id(game = 6304)
-#' whatr_id(date = "2019-06-03")
-#' whatr_id(show = 8006)
-#' @importFrom httr HEAD
-#' @importFrom stringr str_extract
-#' @export
-whatr_id <- function(game = NULL, date = NULL, show = NULL) {
-  if (is.null(game)) {
-    else_arg <- c(!is.null(date), !is.null(show))
-    type <- c("date", "show")[else_arg]
-    input <- c(date, show)
-    response <- httr::HEAD(
-      url = "http://www.j-archive.com/search.php",
-      query = list(search = paste(type, input, sep = ":"))
-    )
-    game <- stringr::str_extract(response$url, "\\d+$")
-  }
-  return(as.character(game))
-}
-
 #' Return the right J! Archive HTML document
 #'
 #' To avoid downloading and reading the J! Archive over and over, this function
